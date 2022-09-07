be3b904f79bbb0321cf5000a5d7efb6687f6d130
gr8pefish <gr8pefish@gmail.com>
2022-09-07 11:57:42 -0700

    Update build file

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ VERSION=1.4.0-pre0.1
 
 mkdir -p tmp/overrides
 
-cp -r config defaultconfigs kubejs openloader worldshape options.txt tmp/overrides/
+cp -r config defaultconfigs kubejs openloader resourcepacks shaderpacks worldshape tmp/overrides/
 
 cp manifest* tmp/manifest.json
 cp modlist* tmp/modlist.html
